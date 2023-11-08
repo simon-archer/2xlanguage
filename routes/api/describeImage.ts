3a1a17c429908b7e7e692e668e58dbbca703f20f
SimonArcher <simonnline@gmail.com>
2023-11-08 14:17:53 +0100

    Update

--- routes/api/describeImage.ts
+++ routes/api/describeImage.ts
@@ -1,44 +1,38 @@
 import { HandlerContext, Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers = {
-  async GET(req: Request, _ctx: HandlerContext) {
-    const url = new URL(req.url);
-    const imageUrl = url.searchParams.get("imageUrl");
-    const lang1 = url.searchParams.get("lang1");
-    const lang2 = url.searchParams.get("lang2");
-
-    if (!imageUrl || !lang1 || !lang2) {
-      return new Response("Missing parameters", { status: 400 });
-    }
-
-    const openaiResponse = await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${Deno.env.get("OPENAI_API_KEY")}`,
-      },
-      body: JSON.stringify({
-        model: "gpt-4-vision-preview",
-        messages: [
-          {
-            role: "user",
-            content: [
-              {
-                type: "text",
-                text: "What’s in this image?"
-              },
-              {
-                type: "image_url",
-                image_url: {
-                  url: imageUrl
+    async POST(req: Request, _ctx: HandlerContext) {
+      const body = new Uint8Array(await req.arrayBuffer());
+      const base64Image = btoa(String.fromCharCode(...body));
+  
+      const openaiResponse = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${Deno.env.get("OPENAI_API_KEY")}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-4-vision-preview",
+          messages: [
+            {
+              role: "user",
+              content: [
+                {
+                  type: "text",
+                  text: "What’s in this image?"
+                },
+                {
+                  type: "image",
+                  image: {
+                    url: `data:image/jpeg;base64,${base64Image}`
+                  }
                 }
-              }
-            ]
-          }
-        ],
-        max_tokens: 300
-      }),
-    });
+              ]
+            }
+          ],
+          max_tokens: 300
+        }),
+      });
 
     if (!openaiResponse.ok) {
       return new Response("OpenAI API request failed", { status: 500 });
--- routes/index.tsx
+++ routes/index.tsx
@@ -7,31 +7,26 @@ export default function Home() {
   const handleImageUpload = async (event) => {
     event.preventDefault();
     const file = event.target.elements.file.files[0];
-    const imageUrl = URL.createObjectURL(file);
-
-    const response = await fetch(`/api/describeImage?imageUrl=${encodeURIComponent(imageUrl)}&lang1=en&lang2=es`);
+    const lang1 = "en";
+    const lang2 = "es";
+  
+    const formData = new FormData();
+    formData.append("image", file);
+    formData.append("lang1", lang1);
+    formData.append("lang2", lang2);
+  
+    const response = await fetch("/api/describeImage", {
+      method: "POST",
+      body: formData,
+    });
     const data = await response.json();
-
+  
     // TODO: Display the image description in the two languages
   };
 
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+    <div class="px-4 py-8 mx-auto">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-
         <form onSubmit={handleImageUpload}>
           <input type="file" name="file" accept="image/*" />
           <button type="submit">Upload Image</button>
