735d924114135ab3bb89dc5a21ed0e88059252ec
SimonArcher <simonnline@gmail.com>
2024-06-23 13:40:55 +0200

    Update _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,11 +5,15 @@ export default function App({ Component }: AppProps) {
   return (
     <>
       <Head>
-      <meta charset="utf-8" />
+        <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="/styles.css" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
-        <title>2xlanguage</title>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        >
+        </link>
+        <title>2Caption</title>
       </Head>
       <Component />
     </>
