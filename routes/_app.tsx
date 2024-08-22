6710b4d64b68511764fb4be933608d20b5adb9d4
SimonArcher <simonnline@gmail.com>
2024-08-22 10:51:19 -0700

    Update _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component }: AppProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="/styles.css" />
-        <link rel="icon" href="/icon.svg" />
+        <link rel="icon" href="/logo.svg" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
